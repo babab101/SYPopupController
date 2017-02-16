3dd68a11cb46e6d01132508bc76a6956ebe3502a
ahn soo yeol <sooyeol.ahn@ompworld.com>
2017-02-16 18:23:00 +0900

    -

---
+++ Example/Pods/Target Support Files/FRHyperLabel/FRHyperLabel-umbrella.h
@@ -0,0 +1,9 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "FRHyperLabel.h"
+
+FOUNDATION_EXPORT double FRHyperLabelVersionNumber;
+FOUNDATION_EXPORT const unsigned char FRHyperLabelVersionString[];
+
